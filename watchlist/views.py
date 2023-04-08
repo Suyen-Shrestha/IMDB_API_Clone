8cfba439e16e3f51a6d881dfe018a4b7e3aafb30
Suyen Shrestha <suyencrestha@gmail.com>
2023-04-08 22:20:59 +0545

    feat: added api-endpoint to retreive, update and delete watchlist.

--- watchlist/views.py
+++ watchlist/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.views import APIView
 from .models import WatchList, StreamPlatform
 from .serializers import WatchListSerializer, StreamPlatformSerializer
@@ -36,3 +37,31 @@ class WatchListAPIView(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+        
+
+
+class StreamPlatformDetailAV(APIView):
+
+    def get(self, request, pk):
+        try:
+            platform = StreamPlatform.objects.get(pk=pk)
+        except StreamPlatform.DoesNotExist:
+            return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = StreamPlatformSerializer(
+            platform, context={'request': request})
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        platform = StreamPlatform.objects.get(pk=pk)
+        serializer = StreamPlatformSerializer(platform, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        platform = StreamPlatform.objects.get(pk=pk)
+        platform.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
